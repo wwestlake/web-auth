a340b0d588f3aeb76c39434c4d4abf3b7c76c2f5
Bill Westlake <wwestlake@lagdaemon.com>
2022-01-20 21:11:20 -0500

    Added body and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div className="container dark position-absolute bottom-0 end-0">
+    <div className="container">
         Copyright (C) 2022, William W. Westlake, All Rights Reserved
     </div>
   )
